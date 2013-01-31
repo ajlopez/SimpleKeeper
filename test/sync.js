37471a306d19038e553cc21a33c9430ed700d2f3
ajlopez <webmaster@ajlopez.com>
2013-01-31 11:27:33 -0300

    Get Set Value

--- test/sync.js
+++ test/sync.js
@@ -14,3 +14,12 @@ exports['Get Empty Root Value'] = function (test) {
     test.equal(result, null);
     test.done();
 };
+
+exports['Set and Get Root Value'] = function (test) {
+	var server = sk.createSyncServer();
+    server.setValue('/', 'foo');
+    var result = server.getValue('/');
+    test.ok(result);
+    test.equal(result, 'foo');
+    test.done();
+};
