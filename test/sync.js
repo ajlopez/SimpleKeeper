2f08b221781264f899c02e4d8a75060efc80307e
ajlopez <webmaster@ajlopez.com>
2013-01-31 11:17:56 -0300

    Get Null Value

--- test/sync.js
+++ test/sync.js
@@ -7,3 +7,10 @@ exports['Create Sync Server'] = function (test) {
 	test.equal(typeof server, 'object');
     test.done();
 };
+
+exports['Get Empty Root Value'] = function (test) {
+	var server = sk.createSyncServer();
+    var result = server.getValue('/');
+    test.equal(result, null);
+    test.done();
+};
