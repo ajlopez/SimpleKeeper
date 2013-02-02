355428fce51d639511aa1e1aa9045f2e48f81ed1
ajlopez <webmaster@ajlopez.com>
2013-02-02 07:39:02 -0300

    Refactor test to use simpleinvoke

--- test/server.js
+++ test/server.js
@@ -63,7 +63,7 @@ exports['Set Value Invalid Paths'] = function (test) {
 	var server = sk.createServer();
     
     test.expect(4);
-    
+
     server.setValue(null, 'adam', step1);
     
     function step1(err) {
@@ -120,166 +120,94 @@ exports['Get Value Invalid Paths'] = function (test) {
 exports['Exists on Existent Path and Ascendants'] = function (test) {
     var server = sk.createServer();
     
-    test.expect(7);
+    test.expect(6);
 
-    server.setValue('/user/1', 'adam', step1);
-
-    function step1(err) {
-        test.ok(!err);
-
-        server.exists('/user/1', step2);
-    }
-
-    function step2(err, result) {
-        test.ok(!err);
-        test.equal(result, true);
-
-        server.exists('/user', step3);
-    }
-
-    function step3(err, result) {
-        test.ok(!err);
-        test.equal(result, true);
-
-        server.exists('/', step4);
-    }
-
-    function step4(err, result) {
-        test.ok(!err);
-        test.equal(result, true);
-
-        test.done();
-    }
+    si.invoke(
+        server, server.setValue, ['/user/1', 'adam'],
+        server, server.exists, ['/user/1'],
+        server, server.exists, ['/user'],
+        server, server.exists, ['/'],
+        function (err, results) {
+            test.ok(!err);
+            test.ok(results);
+            test.equal(results.length, 4);
+            test.equal(results[1], true);
+            test.equal(results[2], true);
+            test.equal(results[3], true);
+            test.done();
+        }
+    );
 };
 
 exports['Get Children'] = function (test) {
     var server = sk.createServer();
     
-    test.expect(7);
-    
-    server.setValue('/user/1', 'adam', step1);
-    
-    function step1(err) {
-        test.ok(!err);
-        
-        server.setValue('/user/2', 'eve', step2);
-    }
-    
-    function step2(err) {
-        test.ok(!err);
-        
-        server.getChildren('/user', step3);
-    }
+    test.expect(5);
 
-    function step3(err, result) {
-        test.ok(!err);
-        test.ok(result);
-        test.equal(2, result.length);
-        test.ok(result.indexOf('1') >= 0);
-        test.ok(result.indexOf('2') >= 0);
-
-        test.done();
-    }
+    si.invoke(
+        server, server.setValue, ['/user/1', 'adam'],
+        server, server.setValue, ['/user/2', 'eve'],
+        server, server.getChildren, ['/user'],
+        function (err, results) {
+            test.ok(!err);
+            test.ok(results);
+            test.equal(results.length, 3);
+            test.ok(results[2].indexOf('1') >= 0);
+            test.ok(results[2].indexOf('2') >= 0);
+            test.done();
+        }
+    );
 };
 
 exports['Delete'] = function (test) {
     var server = sk.createServer();
     
-    test.expect(9);
+    test.expect(5);
     
-    server.setValue('/user/1/name', 'adam', step1);
-    
-    function step1(err) {
-        test.ok(!err);
-        server.setValue('/user/1/age', 800, step2);
-    }
-
-    function step2(err) {
-        test.ok(!err);
-        server.setValue('/user/2/name', 'eve', step3);
-    }
-
-    function step3(err) {
-        test.ok(!err);
-        server.setValue('/user/2/age', 700, step4);
-    }
-
-    function step4(err) {
-        test.ok(!err);
-        server.delete('/user/1', step5);
-    }
-
-    function step5(err) {
-        test.ok(!err);
-        server.getChildren('/user', step6);
-    }
-
-    function step6(err, result) {
-        test.ok(!err);
-        test.ok(result);
-        test.equal(result.length, 1);
-        test.equal(result[0], '2');
-
-        test.done();
-    }
+    si.invoke(
+        server, server.setValue, ['/user/1/name', 'adam'],
+        server, server.setValue, ['/user/1/age', 800],
+        server, server.setValue, ['/user/2/name', 'eve'],
+        server, server.setValue, ['/user/2/age', 700],
+        server, server.delete, ['/user/1'],
+        server, server.getChildren, ['/user'],
+        function (err, results) {
+            test.ok(!err);
+            test.ok(results);
+            test.equal(results.length, 6);
+            test.equal(results[5].length, 1);
+            test.equal(results[5][0], '2');
+            test.done();
+        }
+    );
 };
 
 exports['Delete Root'] = function (test) {
     var server = sk.createServer();
     
-    test.expect(13);
+    test.expect(7);
     
-    server.setValue('/user/1/name', 'adam', step1);
-    
-    function step1(err) {
-        test.ok(!err);
-        server.setValue('/user/1/age', 800, step2);
-    }
-
-    function step2(err) {
-        test.ok(!err);
-        server.setValue('/user/2/name', 'eve', step3);
-    }
-
-    function step3(err) {
-        test.ok(!err);
-        server.setValue('/user/2/age', 700, step4);
-    }
-
-    function step4(err) {
-        test.ok(!err);
-        server.delete('/', step5);
-    }
-
-    function step5(err) {
-        test.ok(!err);
-        server.exists('/user/1', step6);
-    }
-
-    function step6(err, result) {
-        test.ok(!err);
-        test.equal(result, false);
-        server.exists('/user/2', step7);
-    }
-
-    function step7(err, result) {
-        test.ok(!err);
-        test.equal(result, false);
-        server.exists('/user', step8);
-    }
-
-    function step8(err, result) {
-        test.ok(!err);
-        test.equal(result, false);
-        server.exists('/', step9);
-    }
-
-    function step9(err, result) {
-        test.ok(!err);
-        test.equal(result, false);
-
-        test.done();
-    }
+    si.invoke(
+        server, server.setValue, ['/user/1/name', 'adam'],
+        server, server.setValue, ['/user/1/age', 800],
+        server, server.setValue, ['/user/2/name', 'eve'],
+        server, server.setValue, ['/user/2/age', 700],
+        server, server.delete, ['/'],
+        server, server.exists, ['/user/1'],
+        server, server.exists, ['/user/2'],
+        server, server.exists, ['/user'],
+        server, server.exists, ['/'],
+        function (err, results) {
+            test.ok(!err);
+            test.ok(results);
+            test.equal(results.length, 9);
+            test.equal(results[5], false);
+            test.equal(results[6], false);
+            test.equal(results[7], false);
+            test.equal(results[8], false);
+            test.done();
+        }
+    );
 };
 
 exports['Exists Invalid Paths'] = function (test) {
