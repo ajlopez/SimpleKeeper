0bac92d1434beb4bad4e81903d46c1340ff51cdc
ajlopez <webmaster@ajlopez.com>
2013-02-12 07:47:09 -0300

    set and get value remote test

--- test/client.js
+++ test/client.js
@@ -34,3 +34,25 @@ exports['Get Empty Root Value'] = function (test) {
     });
 };
 
+exports['Set and Get Root Value'] = function (test) {
+	var server = sk.createServer();
+    server.listen(3000);
+    
+    test.expect(4);
+
+    var client = sk.createClient(3000, function(remote) {
+        test.ok(remote);
+        si.invoke(
+            remote, remote.setValue, ['/', 'foo'],
+            remote, remote.getValue, ['/'],
+            function (err, results) {
+                test.ok(!err);
+                test.ok(results);
+                test.equal(results[1], 'foo');
+                client.end();
+                server.close();
+                test.done();
+            }
+        );
+    });
+};
